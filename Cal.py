b04447375d56057e11890c0bb3fc4d1d912803d8
Manoj <saimanojpacha@gmail.com>
2026-02-18 11:02:01 +0530

    First Version

---
+++ Cal.py
@@ -0,0 +1,14 @@
+def multiply(a,b):
+    return a*b
+
+def addition(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def divide(a,b):
+    return a/b
+
+def modulus(a,b):
+    return a%b
---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from Cal import *
+
+st.header('Calculator')
+st.write("This is the app for calculator")
+
+a = st.number_input("Enter first number")
+b = st.number_input("Enter second number")
+
+operator = st.selectbox("Operator",['+','-','*','/','%'])
+
+submit = st.button("Answer")
+
+st.write(submit)
+
+if submit:
+    if operator=='+':
+        ans = addition(a,b)
+    elif operator=='-':
+        ans = subtract(a,b)
+    elif operator=='*':
+        ans = multiply(a,b)
+    elif operator=='/':
+        ans = divide(a,b)
+    elif operator=='%':
+        ans = modulus(a,b)
+
+    st.write(f"The answer is {ans}")
+
