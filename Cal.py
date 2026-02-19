96099c2c6e3b9c5da8c4ee60a193ffd34b5dfa3f
Ramteja Krovvidi <ramtejakrovvidi618@gmail.com>
2026-02-19 11:14:13 +0530

    commited

--- Cal.py
+++ Cal.py
@@ -8,7 +8,10 @@ def subtract(a,b):
     return a-b
 
 def divide(a,b):
-    return a/b
+    if b==0:
+        print('cannot divide by 0')
+    else:
+      return a/b
 
 def modulus(a,b):
     return a%b
--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("This is the app for calculator")
 a = st.number_input("Enter first number")
 b = st.number_input("Enter second number")
 
-operator = st.selectbox("Operator",['+','-','*','/','%','power'])
+operator = st.selectbox("Operator",['+','-','*','/','%','power','square'])
 
 submit = st.button("Answer")
 
@@ -24,6 +24,8 @@ if submit:
         ans = modulus(a,b)
     elif operator=='power':
         ans = power(a,b)
+    elif operator=='square':
+        ans = squares(a)
 
     st.write(f"The answer is {ans}")
 
